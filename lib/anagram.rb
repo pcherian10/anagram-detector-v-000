c28cbb2aedd574dd484761d748812b77f35355fb
Paul <pcherian10@gmail.com>
2018-05-07 20:08:29 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,12 @@
 class Anagram
-
 attr_accessor :word
 
 def initialize (word)
-  @word = word
+  @word = word.split("")
 end
 
 def match (test_array)
-  test_array.each do |test_word|
-    test_word.split("").each do |letter|
-      
-
-
-
+  test_array.each  {|test_word| test_word if @word.sort == test_word.sort}
 end
 
 end
